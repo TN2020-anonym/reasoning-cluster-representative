dd153ca4d1d71d331d679ff7c877fcc62eec01df
Thanh Ngoc Nguyen <nthanh0203@gmail.com>
2018-12-28 05:08:27 +0700

    Allow to determine separator and delimiter of input files separately

--- utils/DataReader.py
+++ utils/DataReader.py
@@ -7,15 +7,16 @@ class DataReader():
         self.data_files = data_files
         self.selected_cols = selected_cols
         self.data = np.zeros((1, len(selected_cols)))
+        self.isInit = False
 
-    def __read_file(self, file_idx):
+    def __read_file(self, file_idx, separator = ',', delimiter = ','):
         data_file = self.data_files[file_idx]
-        dataset = pd.read_csv(data_file, delimiter='\t')
+        dataset = pd.read_csv(data_file, delimiter=delimiter)
         dataset = dataset.fillna(0)
         data_read = np.hstack([dataset.values[:, col].reshape((-1, 1)) for col in self.selected_cols])
         self.data = np.vstack((self.data, data_read))
 
-    def read(self, file_idx = None):
+    def read(self, file_idx = None, separator = ',', delimiter = ','):
         if file_idx is None:
             for id in range(len(self.data_files)):
                 self.__read_file(id)
@@ -23,3 +24,15 @@ class DataReader():
             self.__read_file(file_idx)
         
         self.data = self.data[1:, :]
+
+    def getData(self):
+        return self.data
+
+    def append(self, data):
+        self.data = np.vstack((self.data, data))
+
+        if self.isInit == False:
+            self.data = self.data[1:, :]
+            self.isInit = True
+
+
